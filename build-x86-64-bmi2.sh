429984c1d38fd9168831b371bcd8e5c4fd8f960e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-16 21:09:20 +0100

    define __DATE__ for reproducible build

--- build-x86-64-bmi2.sh
+++ build-x86-64-bmi2.sh
@@ -4,6 +4,6 @@ cd official/src
 rm -f nn-*.nnue
 cp ../../nn-*.nnue .
 make clean
-CXXFLAGS=-DNNUE_EMBEDDING_OFF make ARCH=x86-64-bmi2 profile-build
+CXXFLAGS="-DNNUE_EMBEDDING_OFF -D__DATE__='\"Nov 16 2020\"'" make ARCH=x86-64-bmi2 profile-build
 make strip
 cp stockfish ../../stockfish-x86-64-bmi2
