040ebeeec30293ba05ddaf62f166ca1c73e916b3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-16 21:01:37 +0100

    first profile-build

--- build-x86-64-bmi2.sh
+++ build-x86-64-bmi2.sh
@@ -4,6 +4,6 @@ cd official/src
 rm -f nn-*.nnue
 cp ../../nn-*.nnue .
 make clean
-CXXFLAGS=-DNNUE_EMBEDDING_OFF make ARCH=x86-64-bmi2 build
+CXXFLAGS=-DNNUE_EMBEDDING_OFF make ARCH=x86-64-bmi2 profile-build
 make strip
 cp stockfish ../../stockfish-x86-64-bmi2
