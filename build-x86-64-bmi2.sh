9dbfcc28e015a7ec712ac687a4b000e598a7e237
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-16 23:57:15 +0100

    import artifacts from github actions

--- build-x86-64-bmi2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-cd official/src
-rm -f nn-*.nnue
-cp ../../nn-*.nnue .
-make clean
-CXXFLAGS="-DNNUE_EMBEDDING_OFF -D__DATE__='\"Nov 16 2020\"'" make ARCH=x86-64-bmi2 profile-build
-make strip
-cp stockfish ../../stockfish-x86-64-bmi2
